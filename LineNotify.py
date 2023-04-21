93d0e21c22921633efce6765841fc5d7906dd23e
anusornp <anusornp@revolictech.com>
2023-04-21 18:11:32 +0700

    revise code

--- LineNotify.py
+++ LineNotify.py
@@ -6,19 +6,31 @@ class LineNotify:
         self.headers = {'Authorization':'Bearer '+notify_token}
 
     def Send_Text(self, text):
-        session_post = requests.post(self.url, headers=self.headers , data = {'message':text})
-        # print(session_post.text)
+        try:
+            session_post = requests.post(self.url, headers=self.headers , data = {'message':text})
+            # print(session_post.text)
+        except Exception as ex:
+            print(ex)
 
     def Send_Image(self, text, image_path):
-        file_img = {'imageFile': open(image_path, 'rb')}
-        session = requests.Session()
-        session_post = session.post(self.url, headers=self.headers, files=file_img, data= {'message': text})
-        # print(session_post.text)
+        try:
+            file_img = {'imageFile': open(image_path, 'rb')}
+            session = requests.Session()
+            session_post = session.post(self.url, headers=self.headers, files=file_img, data= {'message': text})
+            # print(session_post.text)
+        except Exception as ex:
+            print(ex)
 
     def Send_Sticker(self, text, stickerPackageId, stickerId):
-        session_post = requests.post(self.url, headers=self.headers, data= {'message': text,'stickerPackageId': stickerPackageId, 'stickerId': stickerId})
-        # print(session_post.text)
+        try:
+            session_post = requests.post(self.url, headers=self.headers, data= {'message': text,'stickerPackageId': stickerPackageId, 'stickerId': stickerId})
+            # print(session_post.text)
+        except Exception as ex:
+            print(ex)
 
     def Send_Emoji(self, text_emoji):
-        session_post = requests.post(self.url, headers=self.headers , data = {'message':text_emoji})
-        # print(session_post.text)
+        try:
+            session_post = requests.post(self.url, headers=self.headers , data = {'message':text_emoji})
+            # print(session_post.text)
+        except Exception as ex:
+            print(ex)
